1e94af9ec414798211e2db97509c70eeccc15c06
Hottab13 <alekseev.a@hotmail.com>
2022-03-20 00:35:31 +0300

    0_10

--- controllers/authentication-controller.js
+++ controllers/authentication-controller.js
@@ -20,17 +20,17 @@ const getLogin = (req, res) => {
                 result._id
             );
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', true);
                 res.status(200).json(token)
             } else {
                 handlErr("Ошибка! Логин или пароль не верны!", res.status(500))
