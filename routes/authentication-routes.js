208ef8bd814775cfc5f2f5dec7db20e8fe999a16
Hottab13 <alekseev.a@hotmail.com>
2022-03-20 02:15:36 +0300

    0_17

--- routes/authentication-routes.js
+++ routes/authentication-routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
+
 const {
   getLogin,
   postRegistration
 } = require('../controllers/authentication-controller');
 
-router.post('/api/login',cors(), getLogin)
+router.post('/api/login', getLogin)
 router.post('/api/registration', postRegistration);
 
 module.exports = router;
