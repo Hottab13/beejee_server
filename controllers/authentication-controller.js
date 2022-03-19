d10ae2e6cb3ecae553d246a81d17da3a021932d9
Hottab13 <alekseev.a@hotmail.com>
2022-03-20 00:33:23 +0300

    0_9

--- controllers/authentication-controller.js
+++ controllers/authentication-controller.js
@@ -3,18 +3,6 @@ const handlErr = require('../utils/handlErr');
 const {generateAccessToken} = require('../utils/authToken');
 
 const getLogin = (req, res) => {
-     // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
     const {
         email,
         password
@@ -31,6 +19,18 @@ const getLogin = (req, res) => {
                 },
                 result._id
             );
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
                 res.status(200).json(token)
             } else {
                 handlErr("Ошибка! Логин или пароль не верны!", res.status(500))
--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const chalk = require('chalk');
 var cors = require('cors');
 
 // use it before all route definitions
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: '*'}));
 
 const ErrorMsg = chalk.bgWhite.red;
 const SuccessMsg = chalk.bgWhite.green;
@@ -33,25 +33,6 @@ app.listen(process.env.PORT, err => {
 app.use(morgan(SuccessMsg(':method :url :status :res[content-length] - :response-time ms')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  
-  // Pass to next layer of middleware
-  next();
-  });
 
 app.use(eventRouter);
 app.use(usertRouter);
