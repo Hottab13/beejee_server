3ee8eeb1c61cccd240f1fd91e042c85c933a3377
Hottab13 <alekseev.a@hotmail.com>
2022-03-20 00:40:15 +0300

    0_11

--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -26,7 +26,21 @@ const getEvents = (req, res) => {
         .sort({
             createdAt: -1
         })
-        .then((events) => res.status(200).json(events))
+        .then((events) => {
+            // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+  
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.header('Access-Control-Allow-Credentials', true);
+            res.status(200).json(events)
+        })
         .catch((err) => handlErr(err.message, res.status(500)))
 }
 const postAddEvent = (req, res) => {
