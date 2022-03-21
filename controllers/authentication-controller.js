9ef49a88d37ea577fe086bd18150276023b67273
Hottab13 <alekseev.a@hotmail.com>
2022-03-21 16:54:53 +0300

    0_20

--- controllers/authentication-controller.js
+++ controllers/authentication-controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const handlErr = require('../utils/handlErr');
 const {generateAccessToken} = require('../utils/authToken');
+const jwt = require('jsonwebtoken');
 
 const getLogin = (req, res) => {
     const {
@@ -18,25 +19,28 @@ const getLogin = (req, res) => {
                     email: req.body.email
                 },
                 result._id
-            );/*
-  // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', '*');
-  
-  // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-  // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header('Access-Control-Allow-Credentials', true);*/
+            );
                 res.status(200).json(token)
             } else {
                 handlErr("Ошибка! Логин или пароль не верны!", res.status(500))
             }
         })
         .catch((err) => handlErr(err.message, res.status(500)))
+}
+const getToken = (req, res) => {
+    const token = req.headers['authorization']
+    //const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return handlErr("Ошибка! Отсутствует токен аунтификации!", res.status(401))
+    jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+            if (err) return handlErr(err.message, res.status(403))
+            User
+                .findOne({
+                    email: user.email
+                })
+                .then((result) => res.status(200).json(result))
+                .catch((err) => handlErr(err.message, res.status(500)))
+        }
+    )
 }
 const postRegistration = (req, res) => {
     const {
@@ -76,5 +80,6 @@ const postRegistration = (req, res) => {
 
 module.exports = {
     getLogin,
-    postRegistration
+    postRegistration,
+    getToken
 }
--- routes/authentication-routes.js
+++ routes/authentication-routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
   getLogin,
-  postRegistration
+  postRegistration,
+  getToken
 } = require('../controllers/authentication-controller');
 
 router.post('/api/login', getLogin)
+router.get('/api/token', getToken)
 router.post('/api/registration', postRegistration);
 
 module.exports = router;
